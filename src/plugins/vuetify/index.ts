30737d297dd9249e34253f50074517d87c6c3a2f
jxxghp <jxxghp@gmail.com>
2023-06-27 21:11:27 +0800

    global components

--- src/plugins/vuetify/index.ts
+++ src/plugins/vuetify/index.ts
@@ -1,9 +1,10 @@
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
 import { VBtn } from 'vuetify/components/VBtn'
+import * as labsComponents from 'vuetify/labs/components'
 import defaults from './defaults'
 import { icons } from './icons'
 import theme from './theme'
-
 // Styles
 import '@core/scss/template/libs/vuetify/index.scss'
 import 'vuetify/styles'
@@ -15,4 +16,8 @@ export default createVuetify({
   defaults,
   icons,
   theme,
+  components: {
+    ...components,
+    ...labsComponents,
+  },
 })
