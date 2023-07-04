e6ad24daed7bf50e1836828ed3c400ccf6667888
jxxghp <jxxghp@gmail.com>
2023-07-04 21:01:39 +0800

    rollback sse

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <script lang="ts" setup>
-import { useToast } from "vue-toast-notification";
-import store from "./store";
-
 const route = useRoute();
-const $toast = useToast();
-
-onMounted(() => {
-  const token = store.state.auth.token;
-  if (token) {
-    const eventSource = new EventSource(
-      `${import.meta.env.VITE_API_BASE_URL}system/message?token=${token}`
-    );
-
-    eventSource.addEventListener("message", (event) => {
-      const message = event.data;
-      $toast.info(message);
-    });
-
-    onBeforeUnmount(() => {
-      eventSource.close();
-    });
-  }
-});
 </script>
 
 <template>
