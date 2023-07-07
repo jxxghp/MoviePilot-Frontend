e8372b8566edc27593256ad236d3e85502f6caea
jxxghp <jxxghp@gmail.com>
2023-07-07 12:52:40 +0800

    remove unuse file

--- src/layouts/components/UserProfile.vue
+++ src/layouts/components/UserProfile.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import router from '@/router';
-import avatar1 from '@images/avatars/avatar-1.png';
-import { useStore } from 'vuex';
+import router from "@/router";
+import avatar1 from "@images/avatars/avatar-1.png";
+import { useStore } from "vuex";
 
 // Vuex Store
 const store = useStore();
@@ -9,36 +9,20 @@ const store = useStore();
 // 执行注销操作
 const logout = () => {
   // 清除登录状态信息
-  store.dispatch('auth/clearToken');
+  store.dispatch("auth/clearToken");
 
   // 重定向到登录页面或其他适当的页面
-  router.push('/login')
-}
+  router.push("/login");
+};
 </script>
 
 <template>
-  <VBadge
-    dot
-    location="bottom right"
-    offset-x="3"
-    offset-y="3"
-    color="success"
-    bordered
-  >
-    <VAvatar
-      class="cursor-pointer"
-      color="primary"
-      variant="tonal"
-    >
+  <VBadge dot location="bottom right" offset-x="3" offset-y="3" color="success" bordered>
+    <VAvatar class="cursor-pointer" color="primary" variant="tonal">
       <VImg :src="avatar1" />
 
       <!-- SECTION Menu -->
-      <VMenu
-        activator="parent"
-        width="230"
-        location="bottom end"
-        offset="14px"
-      >
+      <VMenu activator="parent" width="230" location="bottom end" offset="14px">
         <VList>
           <!-- 👉 User Avatar & Name -->
           <VListItem>
@@ -51,34 +35,22 @@ const logout = () => {
                   offset-y="3"
                   color="success"
                 >
-                  <VAvatar
-                    color="primary"
-                    variant="tonal"
-                  >
+                  <VAvatar color="primary" variant="tonal">
                     <VImg :src="avatar1" />
                   </VAvatar>
                 </VBadge>
               </VListItemAction>
             </template>
 
-            <VListItemTitle class="font-weight-semibold">
-              管理员
-            </VListItemTitle>
+            <VListItemTitle class="font-weight-semibold"> 管理员 </VListItemTitle>
             <VListItemSubtitle>Admin</VListItemSubtitle>
           </VListItem>
           <VDivider class="my-2" />
 
           <!-- 👉 Profile -->
-          <VListItem
-            link
-            to="account-settings"
-          >
+          <VListItem link to="account-setting">
             <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-account-outline"
-                size="22"
-              />
+              <VIcon class="me-2" icon="mdi-account-outline" size="22" />
             </template>
 
             <VListItemTitle>个人中心</VListItemTitle>
@@ -87,11 +59,7 @@ const logout = () => {
           <!-- 👉 FAQ -->
           <VListItem link>
             <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-help-circle-outline"
-                size="22"
-              />
+              <VIcon class="me-2" icon="mdi-help-circle-outline" size="22" />
             </template>
 
             <VListItemTitle>帮助</VListItemTitle>
@@ -103,11 +71,7 @@ const logout = () => {
           <!-- 👉 Logout -->
           <VListItem @click="logout">
             <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-logout"
-                size="22"
-              />
+              <VIcon class="me-2" icon="mdi-logout" size="22" />
             </template>
 
             <VListItemTitle>注销</VListItemTitle>
--- src/pages/account-setting.vue
+++ src/pages/account-setting.vue
@@ -1,59 +1,45 @@
 <script lang="ts" setup>
-import { useRoute } from 'vue-router'
-import AccountSettingsAccount from '@/views/pages/account-settings/AccountSettingsAccount.vue'
-import AccountSettingsNotification from '@/views/pages/account-settings/AccountSettingsNotification.vue'
-import AccountSettingsSecurity from '@/views/pages/account-settings/AccountSettingsSecurity.vue'
+import AccountSettingAccount from "@/views/account-setting/AccountSettingAccount.vue";
+import AccountSettingSecurity from "@/views/account-setting/AccountSettingSecurity.vue";
+import AccountSettingSystem from "@/views/account-setting/AccountSettingSystem.vue";
+import { useRoute } from "vue-router";
 
-const route = useRoute()
+const route = useRoute();
 
-const activeTab = ref(route.params.tab)
+const activeTab = ref(route.params.tab);
 
 // tabs
 const tabs = [
-  { title: 'Account', icon: 'mdi-account-outline', tab: 'account' },
-  { title: 'Security', icon: 'mdi-lock-open-outline', tab: 'security' },
-  { title: 'Notifications', icon: 'mdi-bell-outline', tab: 'notification' },
-]
+  { title: "账户", icon: "mdi-account-outline", tab: "account" },
+  { title: "安全", icon: "mdi-lock-open-outline", tab: "security" },
+  { title: "系统", icon: "mdi-bell-outline", tab: "system" },
+];
 </script>
 
 <template>
   <div>
-    <VTabs
-      v-model="activeTab"
-      show-arrows
-    >
-      <VTab
-        v-for="item in tabs"
-        :key="item.icon"
-        :value="item.tab"
-      >
-        <VIcon
-          size="20"
-          start
-          :icon="item.icon"
-        />
+    <VTabs v-model="activeTab" show-arrows>
+      <VTab v-for="item in tabs" :key="item.icon" :value="item.tab">
+        <VIcon size="20" start :icon="item.icon" />
         {{ item.title }}
       </VTab>
     </VTabs>
     <VDivider />
 
-    <VWindow
-      v-model="activeTab"
-      class="mt-5 disable-tab-transition"
-    >
+    <VWindow v-model="activeTab" class="mt-5 disable-tab-transition">
       <!-- Account -->
       <VWindowItem value="account">
-        <AccountSettingsAccount />
+        <AccountSettingAccount />
       </VWindowItem>
 
       <!-- Security -->
       <VWindowItem value="security">
-        <AccountSettingsSecurity />
+        <AccountSettingSecurity />
       </VWindowItem>
 
-      <!-- Notification -->
-      <VWindowItem value="notification">
-        <AccountSettingsNotification />
+      <!-- System -->
+      <VWindowItem value="system">
+        <AccountSettingSystem />
       </VWindowItem>
     </VWindow>
   </div>
