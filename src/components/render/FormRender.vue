74aca36e71a073ea0d328d7a5d1cd67b024c2e51
jxxghp <jxxghp@gmail.com>
2023-07-24 11:36:21 +0800

    fix plugin form

--- src/components/render/FormRender.vue
+++ src/components/render/FormRender.vue
@@ -4,21 +4,25 @@ import { type PropType, ref } from 'vue'
 // 组件配置
 interface RenderProps {
   component: string
-  content: any
-  props: any
+  content?: any
+  props?: any
 }
 
 // 输入参数
 const elementProps = defineProps({
-  config: Array as PropType<RenderProps[]>,
+  config: Object as PropType<RenderProps>,
 })
 
 // 配置表单
-const formItems = ref(elementProps.config)
+const formItem = ref<RenderProps>(elementProps.config || {
+  component: 'div',
+  props: {},
+  content: [],
+})
 </script>
 
 <template>
-  <Component :is="item.component" v-for="(item, index) in formItems" :key="index" v-bind="item.props">
-    <FormRender v-for="(innerItem, innerIndex) in (item.content || [])" :key="innerIndex" :config="innerItem" />
+  <Component :is="formItem.component" v-bind="formItem.props">
+    <FormRender v-for="(innerItem, innerIndex) in (formItem.content || [])" :key="innerIndex" :config="innerItem" />
   </Component>
 </template>
