7d5e30a5cacd033f77a8094e0f5f6171ea01df0d
jxxghp <jxxghp@gmail.com>
2023-07-24 10:43:04 +0800

    fix

--- src/components/render/FormRender.vue
+++ src/components/render/FormRender.vue
@@ -1,22 +1,24 @@
 <script lang="ts" setup>
 import { type PropType, ref } from 'vue'
 
+// 组件配置
 interface RenderProps {
   component: string
   content: any
+  props: any
 }
 
 // 输入参数
-const props = defineProps({
+const elementProps = defineProps({
   config: Array as PropType<RenderProps[]>,
 })
 
 // 配置表单
-const formItems = ref(props.config)
+const formItems = ref(elementProps.config)
 </script>
 
 <template>
-  <Component :is="item.component" v-for="(item, index) in formItems" :key="index">
+  <Component :is="item.component" v-for="(item, index) in formItems" :key="index" v-bind="item.props">
     <FormRender v-for="(innerItem, innerIndex) in (item.content || [])" :key="innerIndex" :config="innerItem" />
   </Component>
 </template>
