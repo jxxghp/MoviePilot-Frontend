9c5ef8f5b4976e764e89b777079278721f0cf6e4
jxxghp <jxxghp@gmail.com>
2023-09-01 17:17:07 +0800

    fix nodatafound page

--- src/components/NoDataFound.vue
+++ src/components/NoDataFound.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
-import { useTheme } from 'vuetify'
 import miscpose from '@images/pages/pose-fs-9.png'
-import miscMaskDark from '@images/pages/misc-mask-dark.png'
-import miscMaskLight from '@images/pages/misc-mask-light.png'
-import tree from '@images/pages/tree.png'
 
 const props = defineProps<Props>()
-
-const vuetifyTheme = useTheme()
-
-const authThemeMask = computed(() => {
-  return vuetifyTheme.global.name.value === 'light' ? miscMaskLight : miscMaskDark
-})
 
 interface Props {
   errorCode?: string
@@ -21,7 +11,7 @@ interface Props {
 </script>
 
 <template>
-  <div class="misc-wrapper">
+  <div class="flex flex-col">
     <ErrorHeader
       :error-code="props.errorCode"
       :error-title="props.errorTitle"
@@ -29,7 +19,7 @@ interface Props {
     />
 
     <!-- 👉 Image -->
-    <div class="misc-avatar text-center">
+    <div class="text-center">
       <VImg
         :src="miscpose"
         class="mx-auto pt-10"
@@ -38,40 +28,8 @@ interface Props {
       />
       <slot name="button" />
     </div>
-
-    <!-- 👉 Footer -->
-    <VImg
-      :src="tree"
-      class="misc-footer-tree d-none d-lg-block"
-    />
-
-    <VImg
-      :src="authThemeMask"
-      class="misc-footer-img d-none d-md-block"
-    />
   </div>
 </template>
 
 <style lang="scss">
-@use "@configured-variables" as variables;
-@use '@core/scss/pages/misc.scss';
-
-.misc-wrapper {
-  position: relative;
-
-  .misc-footer-tree {
-    position: fixed;
-    z-index: 1;
-    inline-size: 15.625rem;
-    inset-block-end: 3.5rem;
-    inset-inline-start: 0.375rem;
-    left: variables.$layout-vertical-nav-width;
-  }
-
-  .misc-footer-img {
-    position: fixed;
-    inline-size: 100%;
-    inset-block-end: 0;
-  }
-}
 </style>
