2c3ecfeb6ff38c2394a33816b91cd5c3a7e74570
Allen <hotlcc@qq.com>
2024-04-15 01:52:17 +0000

    低版本浏览器at函数兼容性调整为应用级生效

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@ import { useTheme } from 'vuetify'
 
 import store from './store'
 
+import { fixArrayAt } from '@/@core/utils/compatibility'
+
+// 修复低版本Safari等浏览器数组不支持at函数的问题
+fixArrayAt()
+
 // 提示框
 const $toast = useToast()
 
