0c37c0149681eb1061672519c575b68bf0faccfe
jxxghp <jxxghp@gmail.com>
2024-05-13 07:06:37 +0800

    feat: add new media cards and components

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,13 @@ import 'vue-toast-notification/dist/theme-bootstrap.css'
 import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar'
 import 'vue3-perfect-scrollbar/style.css'
 import DialogCloseBtn from '@/@core/components/DialogCloseBtn.vue'
+import MediaCard from './components/cards/MediaCard.vue'
+import PosterCard from './components/cards/PosterCard.vue'
+import BackdropCard from './components/cards/BackdropCard.vue'
+import PersonCard from './components/cards/PersonCard.vue'
+import MediaInfoCard from './components/cards/MediaInfoCard.vue'
+import TorrentCard from './components/cards/TorrentCard.vue'
+import MediaIdSelector from './components/misc/MediaIdSelector.vue'
 import { fixArrayAt } from '@/@core/utils/compatibility'
 
 // 修复低版本Safari等浏览器数组不支持at函数的问题
@@ -34,6 +41,13 @@ app
   .component('VAceEditor', VAceEditor)
   .component('VApexChart', VueApexCharts)
   .component('VDialogCloseBtn', DialogCloseBtn)
+  .component('VMediaCard', MediaCard)
+  .component('VPosterCard', PosterCard)
+  .component('VBackdropCard', BackdropCard)
+  .component('VPersonCard', PersonCard)
+  .component('VMediaInfoCard', MediaInfoCard)
+  .component('VTorrentCard', TorrentCard)
+  .component('VMediaIdSelector', MediaIdSelector)
 
 // 注册插件
 app
