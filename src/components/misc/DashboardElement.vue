33d2a396ce892cc6782d7a5ac98bd28234c21d6b
jxxghp <jxxghp@gmail.com>
2024-05-17 10:54:19 +0800

    仪表板支持自定义标题

--- src/components/misc/DashboardElement.vue
+++ src/components/misc/DashboardElement.vue
@@ -46,7 +46,7 @@ onUnmounted(() => {
         <VIcon class="cursor-move">mdi-drag</VIcon>
       </template>
       <VCardTitle>
-        {{ props.config?.name }}
+        {{ props.config?.attrs?.title ?? props.config?.name }}
       </VCardTitle>
       <VCardSubtitle v-if="props.config?.attrs?.subtitle"> {{ props.config?.attrs?.subtitle }}</VCardSubtitle>
     </VCardItem>
