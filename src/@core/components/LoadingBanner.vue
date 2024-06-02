d6b17debb4e32f2cf1d8b40890d530b1bbb52885
jxxghp <jxxghp@gmail.com>
2024-06-02 21:27:24 +0800

    fix loading banner

--- src/@core/components/LoadingBanner.vue
+++ src/@core/components/LoadingBanner.vue
@@ -1,28 +1,15 @@
 <script lang="ts" setup>
-  // 定义输入参数
-  const props = defineProps({
-    progress: Number,
-    text: String
-  })
+// 定义输入参数
+const props = defineProps({
+  progress: Number,
+  text: String,
+})
 </script>
 
 <template>
-  <div
-    class="w-full text-center text-gray-500 text-sm flex flex-col items-center"
-  >
-    <VProgressCircular
-      v-if="!props.text"
-      size="48"
-      indeterminate
-      color="primary"
-    />
-    <VProgressCircular 
-      v-if="props.progress" 
-      class="mb-3" 
-      color="primary" 
-      :model-value="props.progress" 
-      size="64" 
-      />
+  <div class="w-full text-center text-gray-500 text-sm flex flex-col items-center">
+    <VProgressCircular v-if="!props.text || !props.progress" size="48" indeterminate color="primary" />
+    <VProgressCircular v-if="props.progress" class="mb-3" color="primary" :model-value="props.progress" size="64" />
     <span>{{ props.text }}</span>
   </div>
 </template>
