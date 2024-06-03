e3d0e10778a8f466a9b9a715dd738addef9c679e
Allen <hotlcc@qq.com>
2024-06-03 11:39:58 +0800

    还愿pageRange的value类型为数字，解决分页下拉选中状态丢失问题

--- src/views/reorganize/TransferHistoryView.vue
+++ src/views/reorganize/TransferHistoryView.vue
@@ -62,12 +62,12 @@ const headers = [
 ]
 
 const pageRange = [
-  { title: '25', value: '25' },
-  { title: '50', value: '50' },
-  { title: '100', value: '100' },
-  { title: '500', value: '500' },
-  { title: '1000', value: '1000' },
-  { title: 'All', value: '-1' },
+  { title: '25', value: 25 },
+  { title: '50', value: 50 },
+  { title: '100', value: 100 },
+  { title: '500', value: 500 },
+  { title: '1000', value: 1000 },
+  { title: 'All', value: -1 },
 ]
 
 // 数据列表
