83e199c1ea28c1cf7deeea294b86ce44dd9adc4f
jxxghp <jxxghp@gmail.com>
2024-09-11 08:05:15 +0800

    refactor: fix media server libraries

--- src/views/dashboard/MediaServerLibrary.vue
+++ src/views/dashboard/MediaServerLibrary.vue
@@ -1,38 +1,56 @@
 <script setup lang="ts">
 import api from '@/api'
-import type { MediaServerPlayItem } from '@/api/types'
+import type { MediaServerConf, MediaServerLibrary } from '@/api/types'
 import LibraryCard from '@/components/cards/LibraryCard.vue'
 
 // 媒体库列表
-const libraryList = ref<MediaServerPlayItem[]>([])
+const libraryList = ref<MediaServerLibrary[]>([])
+
+// 所有媒体服务器设置
+const mediaServers = ref<MediaServerConf[]>([])
+
+// 调用API查询媒体服务器设置
+async function loadMediaServerSetting() {
+  try {
+    const result: { [key: string]: any } = await api.get('system/setting/MediaServers')
+    mediaServers.value = result.data?.value ?? []
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 // 调用API查询
-async function loadLibrary() {
+async function loadLibrary(server: string) {
   try {
-    libraryList.value = await api.get('mediaserver/library')
+    const result: MediaServerLibrary[] = await api.get('mediaserver/library', { params: { server } })
+    if (result && result.length > 0) {
+      libraryList.value = libraryList.value.concat(result)
+    }
   } catch (e) {
     console.log(e)
   }
 }
 
-onMounted(() => {
-  loadLibrary()
+onMounted(async () => {
+  await loadMediaServerSetting()
+  for (const server of mediaServers.value) {
+    loadLibrary(server.name)
+  }
 })
 </script>
 
 <template>
-  <VHover>
+  <VHover v-if="libraryList.length > 0">
     <template #default="hover">
       <VCard v-bind="hover.props">
         <VCardItem>
           <template #append>
             <VIcon class="cursor-move" v-if="hover.isHovering">mdi-drag</VIcon>
           </template>
-          <VCardTitle >我的媒体库</VCardTitle>
+          <VCardTitle>我的媒体库</VCardTitle>
         </VCardItem>
-
-        <div v-if="libraryList.length > 0" class="grid gap-4 grid-backdrop-card mx-3" tabindex="0">
-          <LibraryCard v-for="data in libraryList" :key="data.id" :media="data" height="10rem" />
+        <div class="grid gap-4 grid-backdrop-card mx-3" tabindex="0">
+          <LibraryCard v-for="item in libraryList" :key="item.id" :media="item" height="10rem" />
         </div>
       </VCard>
     </template>
